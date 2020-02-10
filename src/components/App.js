224ae2c163a84d3070ed46eec83ffb3fc55dbe9f
AaronMullan <aaroncmullan@gmail.com>
2020-02-10 15:56:17 -0800

    fresh code from dev

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import ChartSandbox from './sandbox/chartSandbox';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    < ChartSandbox />
+  );
 }
-  
