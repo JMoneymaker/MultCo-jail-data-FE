eba8b02e5f4e366595f58188e2a4618069e2945b
AaronMullan <aaroncmullan@gmail.com>
2020-02-11 14:11:19 -0800

    detentions by race rendering

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,18 @@ import HomePage from '../components/HomePage';
 import PersonsPage from '../components/PersonsPage';
 import DetentionsPage from '../components/DetentionsPage';
 import DeveloperPage from '../components/DeveloperPage';
+import ExampleChart from './charts/DetetionsByGender';
+import DetentionsByRaceChart from './charts/DetentionsByRace';
 
 export default function App() {
   return (
-    <Router>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/persons' component={PersonsPage} />
-      <Route path='/detentions' component={DetentionsPage} />
-      <Route path='/developers' component={DeveloperPage} />
-    </Router>
+    // <Router>
+    //   <Route exact path='/' component={HomePage} />
+    //   <Route path='/persons' component={PersonsPage} />
+    //   <Route path='/detentions' component={DetentionsPage} />
+    //   <Route path='/developers' component={DeveloperPage} />
+    // </Router>
+    <DetentionsByRaceChart />
   );
 }
   
