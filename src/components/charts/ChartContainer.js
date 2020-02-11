b50c4476dc04b34a898e558ca946b3e1ab03a6ef
AaronMullan <aaroncmullan@gmail.com>
2020-02-11 15:04:25 -0800

    added population by race chart

--- src/components/charts/ChartContainer.js
+++ src/components/charts/ChartContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import DetentionsByRaceChart from './DetentionsByRace';
 import DetentionsByGenderChart from './DetetionsByGender';
+import PopulationByRaceChart from './PopulationByRace';
 
 const Charts = () => {
 
   return (
     <main>
+      <PopulationByRaceChart />
       <DetentionsByRaceChart />
       <DetentionsByGenderChart />
     </main>
