77fddd704fe26c4de408ed2e0caa2c19d7da8ae5
Nick <nickbrimmer@gmail.com>
2020-02-10 17:31:24 -0800

    initial styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,17 +4,28 @@ import Header from './Header';
 import SideBar from './SideBar';
 import DynamicDataDisplay from './DynamicDataDisplay';
 import useAverageDetention from '../hooks/useAverageDetention';
+import styles from './HomePage.css';
 
 const HomePage = () => {
 
   return (
-    <>
-      <Header />
-      <SideBar />
-      <DynamicDataDisplay hook={useAverageDetention}>Average Detention</DynamicDataDisplay>
-      <h1>Hello, World!</h1>
+    <container className={styles.pageContainer} >
 
-    </>
+      <div className={styles.Header}>
+        <Header />
+      </div>
+      <div className={styles.sideBar}>
+        <SideBar />
+      </div>
+      <section className={styles.bodyContainer}>
+
+        <div className={styles.dynamicDataDisplay}>
+          <DynamicDataDisplay hook={useAverageDetention}>Average Detention</DynamicDataDisplay>
+        </div>
+
+        <h1>Hello, World!</h1>
+      </section>
+    </container>
   );
 
 };
