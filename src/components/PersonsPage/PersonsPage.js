4e56eb21c96727774b024cc8939879121b458a44
Tali Avni <tali@aristo7.com>
2020-03-05 14:42:40 -0800

    adjusted form and button style

--- src/components/PersonsPage/PersonsPage.js
+++ src/components/PersonsPage/PersonsPage.js
@@ -27,27 +27,29 @@ const PersonsPage = () => {
     <>
       <Header />
       <SideBar />
-      <h1 className={styles.h1}>Sort Bookings By Age, Race, and Gender</h1>
-      <div className={styles.QueryControls}>
+      <div className={styles.pageBody}>
+        <h1 className={styles.h1}>Sort all Bookings By Age, Race, and Gender</h1>
+        <div className={styles.QueryControls}>
         Minimum age: <input type="text" value={minAge} onChange={({ target }) => setMinAge(target.value)} />
         Maximum Age: <input type="text" value={maxAge} onChange={({ target }) => setMaxAge(target.value)} />
-        Gender:<select onChange={({ target }) => setGender(target.value)}>
-          <option value="">Any</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-        </select>
-        Race:<select onChange={({ target }) => setRace(target.value)}>
-          <option value="">Any</option>
-          <option value="Black">Black</option>
-          <option value="Hispanic">Hispanic</option>
-          <option value="P">Pacific Islander</option>
-          <option value="Asian">Asian</option>
-          <option value="White">White</option>
-          <option value="Native American or Alaskan">Native American or Alaskan</option>
-          <option value="Unknown">Unknown</option>
-        </select>
+        Gender: <select onChange={({ target }) => setGender(target.value)}>
+            <option value="">Any</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        Race: <select onChange={({ target }) => setRace(target.value)}>
+            <option value="">Any</option>
+            <option value="Black">Black</option>
+            <option value="Hispanic">Hispanic</option>
+            <option value="P">Pacific Islander</option>
+            <option value="Asian">Asian</option>
+            <option value="White">White</option>
+            <option value="Native American or Alaskan">Native American or Alaskan</option>
+            <option value="Unknown">Unknown</option>
+          </select>
+        </div>
+        <button onClick={handleClick}>Search</button>
       </div>
-      <button onClick={handleClick}>Submit query</button>
       <table className={styles.tbody}>
         <tbody  >
           {persons.map((person) => <PersonRow key={person._id} person={person}/>)}
