3639d76aad97a4bda1fb407287b91ae24b2afaf2
AaronMullan <aaroncmullan@gmail.com>
2020-03-11 21:03:42 -0700

    added the word exact to one NavLink

--- src/components/common/SideBar.js
+++ src/components/common/SideBar.js
@@ -8,7 +8,7 @@ const SideBar = () => {
   return (
     <div className={styles.SideBar}>
       <section className={styles.navContainer} >
-        <NavLink activeClassName={styles.active} to='/'>Home</NavLink>
+        <NavLink activeClassName={styles.active} exact to='/'>Home</NavLink>
         <NavLink activeClassName={styles.active} to='/detentions'>Detentions</NavLink>
         <NavLink activeClassName={styles.active} to='/persons'>Persons</NavLink>
         <NavLink activeClassName={styles.active} to='/developers'>Developers</NavLink>
