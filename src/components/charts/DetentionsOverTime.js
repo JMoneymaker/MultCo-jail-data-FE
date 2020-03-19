d9234cb982b4cdbbe3f868d4e479d208898578b3
AaronMullan <aaroncmullan@gmail.com>
2020-03-19 07:24:37 -0700

    prepearing for actual data

--- src/components/charts/DetentionsOverTime.js
+++ src/components/charts/DetentionsOverTime.js
@@ -8,8 +8,26 @@ const getDetentionsOverTime = () => {
 };
 
 const DetentionsOverTimeChart = () => {
-  const [detentionCount, setDetentionCount] = useState([
-    ['', ''], //optional labels for axes
+  let displayData = [];
+
+  const dummyData = [
+    { date: '2020-03-03T03:02:43Z', count: 1000 },
+    { date: '2020-03-10T03:02:43Z', count: 900 },
+    { date: '2020-03-16T03:02:43Z', count: 800 },
+    { date: '2020-03-19T03:02:43Z', count: 500 }
+  ];
+
+  function dataMunger(rawData) { 
+    rawData.map(item => {
+      const container = [];
+      container[0] = item.date.toDateString(),
+      container[1] = item.count;
+      return container;
+    });
+  }
+
+
+  const [detentionCounts, setDetentionCounts] = useState([
     ['January 1', 1000],
     ['March 15', 950],
     ['March 20', 600],
@@ -20,9 +38,11 @@ const DetentionsOverTimeChart = () => {
   // useEffect(() => {
   //   getDetentionsOverTime()
   //     .then(res => {
-        
+  //       displayData = dataMunger(res);
+  //       setDetentionCounts(displayData);
   //     });
   // }, []);
+
 
   return (
     <>
@@ -32,7 +52,8 @@ const DetentionsOverTimeChart = () => {
         height={'400px'}
         chartType="Line"
         loader={<div>Loading Chart</div>}
-        data={detentionCount}
+        data= {[['', ''], //optional labels for axes
+          ...detentionCounts]}
         options={{
           colors: ['#058F7A', '#9161A2', '#13646A', '#1F4763', '#A2506A'],
           legend: { position: 'none' },
@@ -42,5 +63,6 @@ const DetentionsOverTimeChart = () => {
     </>
   );
 };
+
 
 export default DetentionsOverTimeChart;
